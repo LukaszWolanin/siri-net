5418ffd6bc1bd3755b6e7e37f575c436145ae7c6
Łukasz Wolanin <lukasz.wolanin1@gmail.com>
2022-12-15 23:25:54 +0100

    Additional fields

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,17 @@ using System.Xml.Serialization;
 var subscriptionRequest = new SubscriptionRequest();
 subscriptionRequest.MessageIdentifier = new MessageQualifierStructure { Value = Guid.NewGuid().ToString() };
 subscriptionRequest.RequestTimestamp= DateTime.Now;
+subscriptionRequest.SubscriptionContext = new SubscriptionContextStructure()
+{
+    HeartbeatInterval = "PT1M"
+};
+subscriptionRequest.Address = "http://loalhost:1234";
+subscriptionRequest.RequestorRef = new ParticipantRefStructure { Value = Guid.NewGuid().ToString() };
 subscriptionRequest.VehicleMonitoringSubscriptionRequest = new[] {new VehicleMonitoringSubscriptionStructure()
 {
     SubscriberRef = new ParticipantRefStructure { Value= Guid.NewGuid().ToString() },
-    
+    SubscriptionIdentifier = new SubscriptionQualifierStructure { Value = Guid.NewGuid().ToString() },
+    InitialTerminationTime= DateTime.Now.AddDays(1),
 }};
 
 var siri = new Siri
