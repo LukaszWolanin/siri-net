ce326accf0d4f94f28bdaf316e09c6e09c179674
Łukasz Wolanin <lukasz.wolanin1@gmail.com>
2022-12-16 19:50:48 +0100

    cleanup

--- SIRI.Console/Program.cs
+++ SIRI.Console/Program.cs
@@ -6,18 +6,23 @@ using System.Xml.Serialization;
 
 var subscriptionRequest = new SubscriptionRequest();
 subscriptionRequest.MessageIdentifier = new MessageQualifierStructure { Value = Guid.NewGuid().ToString() };
-subscriptionRequest.RequestTimestamp= DateTime.Now;
+subscriptionRequest.RequestTimestamp= DateTime.UtcNow;
 subscriptionRequest.SubscriptionContext = new SubscriptionContextStructure()
 {
     HeartbeatInterval = "PT1M"
 };
-subscriptionRequest.Address = "http://loalhost:1234";
+subscriptionRequest.Address = "https://db80-195-136-19-28.eu.ngrok.io";
 subscriptionRequest.RequestorRef = new ParticipantRefStructure { Value = Guid.NewGuid().ToString() };
 subscriptionRequest.VehicleMonitoringSubscriptionRequest = new[] {new VehicleMonitoringSubscriptionStructure()
 {
     SubscriberRef = new ParticipantRefStructure { Value= Guid.NewGuid().ToString() },
     SubscriptionIdentifier = new SubscriptionQualifierStructure { Value = Guid.NewGuid().ToString() },
-    InitialTerminationTime= DateTime.Now.AddDays(1),
+    InitialTerminationTime= DateTime.UtcNow.AddMinutes(10),
+    VehicleMonitoringRequest = new VehicleMonitoringRequestStructure
+    {
+        RequestTimestamp= DateTime.UtcNow,
+    }
+    
 }};
 
 var siri = new Siri
@@ -37,4 +42,7 @@ var streamWriter = XmlWriter.Create(memoryStream, new()
 serializer.Serialize(streamWriter, siri);
 var result = Encoding.UTF8.GetString(memoryStream.ToArray());
 
+
+var httpClient = new HttpClient();
+var response = await httpClient.PostAsync("https://api.entur.io/realtime/v1/subscribe", new StringContent(Encoding.UTF8.GetString(memoryStream.ToArray()), Encoding.UTF8, "application/xml"));
 Console.WriteLine(result);
